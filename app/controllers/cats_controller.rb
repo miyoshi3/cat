f607e3e90f4466b8140fc5999a4526b939e6c745
Atsuco Miyoshi <a.miyoshi3@icloud.com>
2017-08-18 16:11:12 +0900

    ビュー作成

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,26 @@
+class CatsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def cute
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+
+  get 'cats/cute', to: 'cats#cute'
   resources :cats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
